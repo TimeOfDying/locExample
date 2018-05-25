9788e0d4b775c7be8091deaa2787d84118b73705
Roman <Duncan1roma@gmail.com>
2018-05-25 16:07:39 +0500

    ...

---
+++ CustomResolver.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IocExample
+{
+    public class CustomResolver
+    {
+        private static Dictionary<Type, Type> bindingTypeToType = new Dictionary<Type, Type>();
+        private static Dictionary<Type, Object> bindingTypeToObject = new Dictionary<Type, Object>();
+
+        public void Bind<T,V>()
+        {
+            bindingTypeToType.Add(typeof(T), typeof(V));
+
+        }
+
+        public void BindToObject<T>(Object obj)
+        {
+            bindingTypeToObject.Add(typeof(T), obj);
+
+        }
+
+        public T Get<T>()
+        {
+            var type = typeof(T);
+            return (T)Get(type);
+        }
+
+        private object Get(Type type)
+        {
+            if (bindingTypeToObject.Keys.Contains(type))
+            {
+                return bindingTypeToObject[type];
+            }
+            else
+            {
+                var targetType = DefineType(type);
+                var constructor = Utils.GetSingleConstructor(targetType);
+                var parameters = constructor.GetParameters();
+                List<Object> definedParameters = new List<object>();
+
+                foreach (var parameter in parameters)
+                {
+                    definedParameters.Add(Get(parameter.ParameterType));
+                }
+
+                return Utils.CreateInstance(targetType, definedParameters);
+
+            }
+        }
+
+        private Type DefineType(Type type)
+        {
+            if (bindingTypeToType.Keys.Contains(type))
+            {
+                return bindingTypeToType[type];
+            }
+
+            return type;
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using IocExample.Classes;
+using Ninject;
+using Ninject.Modules;
+using System;
+
+namespace IocExample
+{
+    class Program
+    {
+
+        //-------------------------original
+
+        /*static void Main(string[] args)
+        {
+            var logger = new ConsoleLogger();
+            var sqlConnectionFactory = new SqlConnectionFactory("SQL Connection", logger);
+            var createUserHandler = new CreateUserHandler(new UserService(new QueryExecutor(sqlConnectionFactory), new CommandExecutor(sqlConnectionFactory), new CacheService(logger, new RestClient("API KEY"))), logger);
+
+            createUserHandler.Handle();
+        }*/
+
+
+        //------------------------ninject
+
+        /*static void Main(string[] args)
+        {
+
+            var ninjectKernel = new StandardKernel();
+            ninjectKernel.Bind<ILogger>().To<ConsoleLogger>();
+            ninjectKernel.Bind<IConnectionFactory>().To<SqlConnectionFactory>().InSingletonScope().WithConstructorArgument("sqlConnection", "Hello");
+            ninjectKernel.Bind<RestClient>().ToSelf().WithConstructorArgument("apiKey", "API KEY");
+
+            var createUserHandler = ninjectKernel.Get<CreateUserHandler>();
+            createUserHandler.Handle();
+
+        }*/
+
+        //--------------------custom resolver
+
+        static void Main(string[] args)
+        {
+            var customResolver = new CustomResolver();
+
+            customResolver.Bind<ILogger, ConsoleLogger>();
+            customResolver.BindToObject<IConnectionFactory>(new SqlConnectionFactory("Hello", customResolver.Get<ILogger>()));
+            customResolver.BindToObject<RestClient>(new RestClient("API_KEY"));
+
+            var createUserHandler = customResolver.Get<CreateUserHandler>();
+            createUserHandler.Handle();
+        }
+    }
+}
